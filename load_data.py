92ddeed3c3a79d6d391f3907c839e00636b350a3
SieunPark <sdsa>
2021-02-05 09:54:34 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -4,11 +4,19 @@ import os
 if __name__ =='__main__':
     val_data_link='1WOP_sQsu4vXCY739VGgiWIbHyOozcjHw'
     data_link='1HBu43eBO-vXJsJp8crEp_Iih3_U7QSR2'
-    zip_dest_path=''
-    print("Downloading data...")
-    gdd.download_file_from_google_drive(file_id=data_link,dest_path=os.path.join(zip_dest_path,'data.zip'),unzip=True)
-    gdd.download_file_from_google_drive(file_id=val_data_link,dest_path=os.path.join(zip_dest_path,'val_data.zip'),unzip=True)
 
-    os.remove(os.path.join(zip_dest_path,'data.zip'))
-    os.remove(os.path.join(zip_dest_path,'val_data.zip'))
+    val_data_path='./val_data'
+    data_path='./data'
+    #Create data directory
+    if os.path.isdir(val_data_path):
+        os.makedirs(val_data_path)
+    if os.path.isdir(data_path):
+        os.makedirs(data_path)
+
+    print("Downloading data...")
+    gdd.download_file_from_google_drive(file_id=data_link,dest_path=os.path.join(data_path,'data.zip'),unzip=True)
+    gdd.download_file_from_google_drive(file_id=val_data_link,dest_path=os.path.join(val_data_path,'val_data.zip'),unzip=True)
+
+    os.remove(os.path.join(data_path,'data.zip'))
+    os.remove(os.path.join(val_data_path,'val_data.zip'))
     print("Downloading complete...")
