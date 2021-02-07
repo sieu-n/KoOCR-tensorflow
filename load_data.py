58900acd43e08cfd2684c8259fb66b1f1bd8dbd1
SieunPark <sdsa>
2021-02-07 09:21:32 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -4,8 +4,8 @@ import concurrent.futures
 import os
 import zipfile
 
-def unzip(file):
-    zf.extract(file)
+def unzip(file,p):
+    zf.extract(file,path=p)
 if __name__ =='__main__':
     val_data_link='1WOP_sQsu4vXCY739VGgiWIbHyOozcjHw'
     data_link='1HBu43eBO-vXJsJp8crEp_Iih3_U7QSR2'
@@ -23,13 +23,14 @@ if __name__ =='__main__':
     gdd.download_file_from_google_drive(file_id=val_data_link,dest_path=os.path.join(val_data_path,'val_data.zip'),unzip=False)
 
     print('Unzipping data...')
+    unzip_lambda=lambda f,p:unzip(f,p)
     zf = zipfile.ZipFile(os.path.join(data_path,'data.zip'))    
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(unzip, zf.infolist())
+        executor.map(unzip_lambda(p=data_path), zf.infolist())
     os.remove(os.path.join(data_path,'data.zip'))
 
     zf = zipfile.ZipFile(os.path.join(val_data_path,'val_data.zip'))
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(unzip, zf.infolist())
+        executor.map(unzip_lambda(p=val_data_path), zf.infolist())
     os.remove(os.path.join(val_data_path,'val_data.zip'))
     print("Downloading complete...")
